910e15b7f2ceed5b9978e6ab2cefe34d4082d5d0
Hguedez <hileryguedez@gmail.com>
2020-01-04 19:04:32 -0400

    Falta solucionar error venta_entrada en la vista,detalle venta entrada funciona bien

--- app/Http/Controllers/Detalle_venta_entradaController.php
+++ app/Http/Controllers/Detalle_venta_entradaController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Detalle_venta_entrada;
 use App\Cliente_natural;
+use App\Venta;
+
 use Illuminate\Support\Facades\DB;
 
 class Detalle_venta_entradaController extends Controller
@@ -13,18 +15,34 @@ class Detalle_venta_entradaController extends Controller
     {
         $this->middleware('auth');
     }
+    
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
+    
     public function index(Request $request,$id_entrada,$id_venta_entrada,$id_costo)
-    {
+    {   
+        
         $detalle_entrada=new Detalle_venta_entrada();
         $detalle_entrada->precio=$id_costo;
         $detalle_entrada->fk_venta_entrada=$id_venta_entrada;
         $detalle_entrada->fk_entrada=$id_entrada;
         $detalle_entrada->save();
+        
+        $ventas=new Venta();
+        $ventas->monto_total=$id_costo;
+        $now = new \DateTime();
+        $ventas->fecha_venta=$now->format('d-m-Y');
+        $ventas->fk_cliente_natural=1;
+        $sub = DB::select(DB::raw("SELECT precio_entrada from entrada WHERE id_entrada='$id_entrada'"));
+        $subtotal = $sub[0]->precio_entrada;
+        $monto = DB::select(DB::raw("SELECT precio from detalle_venta_entrada WHERE fk_venta_entrada='$id_venta_entrada'"));
+        $monto_total = $monto[0]->precio;
+        $ventas->save();
+        $id = DB::select(DB::raw("SELECT Max(id_venta) as venta_id from venta"));
+        $id_venta = $id[0]->venta_id;
 
         $detalle_venta_entrada = DB::select(DB::raw("SELECT id_detalle_entrada,precio,
                                                     (select numero_entrada from entrada where id_entrada='$id_entrada'),
@@ -33,7 +51,13 @@ class Detalle_venta_entradaController extends Controller
                                                     (select primer_nombre from cliente_natural where id_cliente_natural=1)
                                                     FROM detalle_venta_entrada WHERE fk_venta_entrada='$id_venta_entrada'and fk_entrada='$id_entrada'"));
 
-        return view ('home.detalle_venta_entrada')->with('detalle_venta_entrada',$detalle_venta_entrada)->with('id_entrada',$id_entrada)->with('id_venta_entrada',$id_venta_entrada)->with('detalle_entrada',$detalle_entrada);
+        return view ('home.misOrdenes')->with('detalle_venta_entrada',$detalle_venta_entrada)
+                                       ->with('id_entrada',$id_entrada)
+                                       ->with('id_venta_entrada',$id_venta_entrada)
+                                       ->with('detalle_entrada',$detalle_entrada)
+                                       ->with('subtotal',$subtotal)
+                                       ->with('monto_total',$monto_total)
+                                       ->with('id_venta',$id_venta);
     }
 
     /**
@@ -97,8 +121,14 @@ class Detalle_venta_entradaController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($id_detalle_entrada)
     {
-        //
+        $detalle=Detalle_venta_entrada::find($id_detalle_entrada);
+        $detalle->delete();
+        $checkT = DB::select(DB::raw("SELECT MAX(id_venta) as id_venta_eliminar  from venta WHERE $id_detalle_entrada is not null "));
+        $id_venta = $checkT[0]->id_venta_eliminar;
+        $venta=Venta::find($id_venta);
+        $venta->delete();
+        return view('home.modelo');
     }
 }

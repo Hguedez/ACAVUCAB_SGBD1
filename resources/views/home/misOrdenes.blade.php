9be8fa86b59d0c9eea7f9196100274f161a2baad
Hguedez <hileryguedez@gmail.com>
2020-01-04 19:06:11 -0400

    Ves tu entrada

--- resources/views/home/misOrdenes.blade.php
+++ resources/views/home/misOrdenes.blade.php
@@ -4,32 +4,22 @@
     <title>ACAVUCAB</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-
-
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-
-
-
-    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
-    <link rel="stylesheet" href="css/animate.css">
-
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="css/magnific-popup.css">
-
-    <link rel="stylesheet" href="css/aos.css">
-
-    <link rel="stylesheet" href="css/ionicons.min.css">
-
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="css/jquery.timepicker.css">
-
-
-    <link rel="stylesheet" href="css/flaticon.css">
-    <link rel="stylesheet" href="css/icomoon.css">
-    <link rel="stylesheet" href="css/style.css">
+    
+    
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{asset('css/open-iconic-bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/animate.css')}}">
+    <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/magnific-popup.css')}}">
+    <link rel="stylesheet" href="{{asset('css/aos.css')}}">
+    <link rel="stylesheet" href="{{asset('css/ionicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/bootstrap-datepicker.css')}}">
+    <link rel="stylesheet" href="{{asset('css/jquery.timepicker.css')}}">
+    <link rel="stylesheet" href="{{asset('css/flaticon.css')}}">
+    <link rel="stylesheet" href="{{asset('css/icomoon.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <style>
        .rojo{
            background: #f4374b;
@@ -153,12 +143,12 @@
 
     <!-- END nav -->
 
-    <div class="hero-wrap hero-bread" style="background-image: url('fondo_cerveza3.jpg');">
+    <div class="hero-wrap hero-bread" style="background-image: url('/fondo_cerveza3.jpg');">
       <div class="container">
         <div class="row no-gutters slider-text align-items-center justify-content-center">
           <div class="col-md-9 ftco-animate text-center">
           	<p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span> <span>Cart</span></p>
-            <h1 class="mb-0 bread">My Cart</h1>
+            <h1 class="mb-0 bread">Mis Entradas</h1>
           </div>
         </div>
       </div>
@@ -173,55 +163,37 @@
 						    <thead class="thead rojo">
 						      <tr class="text-center">
 						        <th>&nbsp;</th>
-						        <th>&nbsp;</th>
-						        <th>Product name</th>
-						        <th>Price</th>
-						        <th>Quantity</th>
-						        <th>Total</th>
+						        <th scope="col">Numero de entrada</th>
+                    <th scope="col">Monto a pagar</th>
+                    <th scope="col">Fecha de la venta</th>
+                    <th scope="col">Del cliente</th>
+						        <!--<th scope="col">Cantidad</th>-->
+                    <th scope="col">Total</th>
+                    <th scope="col">Cancelar</th>
 						      </tr>
 						    </thead>
 						    <tbody>
+                  @foreach ($detalle_venta_entrada as $item)
 						      <tr class="text-center">
-						        <td class="product-remove"><a href="#"><span class="ion-ios-close"></span></a></td>
-
-						        <td class="image-prod"><div class="img" style="background-image:url(images/product-3.jpg);"></div></td>
-
+						        <!--<td class="product-remove"><a href="#"><span class="ion-ios-close"></span></a></td>-->
+						        <td class="image-prod"><div class="img" style="background-image:url(/ticket_event.jpg);"></div></td>
 						        <td class="product-name">
-						        	<h3>Bell Pepper</h3>
-						        	<p>Far far away, behind the word mountains, far from the countries</p>
+                    <h3>{{$item->numero_entrada}}</h3>
+						        <p>Esta entrada es de uso unico y exclusivo para este evento</p>
 						        </td>
-
-						        <td class="price">$4.90</td>
-
-						        <td class="quantity">
-						        	<div class="input-group mb-3">
-					             	<input type="text" name="quantity" class="quantity form-control input-number" value="1" min="1" max="100">
-					          	</div>
-					          </td>
-
-						        <td class="total">$4.90</td>
+                  <td class>{{$item->precio}}</td>
+                  <td>{{$item->fecha}}</td>
+                  <td>{{$item->primer_nombre}}</td>
+                  <td>{{$item->precio}}</td>
+                  <td>
+                    <form action={{ route('Detalle_entradasDestroy',['id_detalle_entrada' => $item->id_detalle_entrada]) }} method="POST" class="d-inline">
+                      @csrf
+                      @method('DELETE')
+                          <button class="btn btn-dark btn-sm tamano letra" type="submit">Cancelar</button>
+                        </form>
+                  </td>
 						      </tr><!-- END TR-->
-
-						      <tr class="text-center">
-						        <td class="product-remove"><a href="#"><span class="ion-ios-close"></span></a></td>
-
-						        <td class="image-prod"><div class="img" style="background-image:url(images/product-4.jpg);"></div></td>
-
-						        <td class="product-name">
-						        	<h3>Bell Pepper</h3>
-						        	<p>Far far away, behind the word mountains, far from the countries</p>
-						        </td>
-
-						        <td class="price">$15.70</td>
-
-						        <td class="quantity">
-						        	<div class="input-group mb-3">
-					             	<input type="text" name="quantity" class="quantity form-control input-number" value="1" min="1" max="100">
-					          	</div>
-					          </td>
-
-						        <td class="total">$15.70</td>
-						      </tr><!-- END TR-->
+                  @endforeach
 						    </tbody>
 						  </table>
 					  </div>
@@ -230,11 +202,11 @@
     		<div class="row justify-content-end">
     			<div class="col-lg-4 mt-5 cart-wrap ftco-animate">
     				<div class="cart-total mb-3">
-    					<h3>Coupon Code</h3>
-    					<p>Enter your coupon code if you have one</p>
+    					<h3>Canjear Puntos</h3>
+    					<p>Paga con puntos si tienes alguno</p>
   						<form action="#" class="info">
 	              <div class="form-group">
-	              	<label for="">Coupon code</label>
+	              	<label for="">Puntos</label>
 	                <input type="text" class="form-control text-left px-3" placeholder="">
 	              </div>
 	            </form>
@@ -243,19 +215,18 @@
     			</div>
     			<div class="col-lg-4 mt-5 cart-wrap ftco-animate">
     				<div class="cart-total mb-3">
-    					<h3>Estimate shipping and tax</h3>
-    					<p>Enter your destination to get a shipping estimate</p>
+    					<h3>Metodo de pago</h3>
   						<form action="#" class="info">
 	              <div class="form-group">
-	              	<label for="">Country</label>
+	              	<label for="">Transferencia</label>
 	                <input type="text" class="form-control text-left px-3" placeholder="">
 	              </div>
 	              <div class="form-group">
-	              	<label for="country">State/Province</label>
+	              	<label for="country">Tarjeta de credito</label>
 	                <input type="text" class="form-control text-left px-3" placeholder="">
 	              </div>
 	              <div class="form-group">
-	              	<label for="country">Zip/Postal Code</label>
+	              	<label for="country">Trajeta de debito</label>
 	                <input type="text" class="form-control text-left px-3" placeholder="">
 	              </div>
 	            </form>
@@ -267,23 +238,19 @@
     					<h3>Cart Totals</h3>
     					<p class="d-flex">
     						<span>Subtotal</span>
-    						<span>$20.60</span>
+              <span>{{$subtotal}} Bs.S</span>
     					</p>
     					<p class="d-flex">
-    						<span>Delivery</span>
-    						<span>$0.00</span>
-    					</p>
-    					<p class="d-flex">
-    						<span>Discount</span>
-    						<span>$3.00</span>
+    						<span>Descuento</span>
+    						<span>0.00 Bs.S</span>
     					</p>
     					<hr>
     					<p class="d-flex total-price">
     						<span>Total</span>
-    						<span>$17.60</span>
+              <span>{{$monto_total}} Bs.S</span>
     					</p>
-    				</div>
-    				<p><a href="checkout.html" class="btn btn-danger py-3 px-4">Proceed to Checkout</a></p>
+            </div>
+          <p><a href="/comprar/{{$id_venta}}/venta/{{$id_entrada}}/entrada" class="btn btn-danger py-3 px-4">Proceed to Checkout</a></p>
     			</div>
     		</div>
 			</div>
@@ -370,59 +337,21 @@
   <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
 
-  <script src="js/jquery.min.js"></script>
-  <script src="js/jquery-migrate-3.0.1.min.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/jquery.easing.1.3.js"></script>
-  <script src="js/jquery.waypoints.min.js"></script>
-  <script src="js/jquery.stellar.min.js"></script>
-  <script src="js/owl.carousel.min.js"></script>
-  <script src="js/jquery.magnific-popup.min.js"></script>
-  <script src="js/aos.js"></script>
-  <script src="js/jquery.animateNumber.min.js"></script>
-  <script src="js/bootstrap-datepicker.js"></script>
-  <script src="js/scrollax.min.js"></script>
+  <script src="{{asset('js/jquery.min.js')}}"></script>
+  <script src="{{asset('js/jquery-migrate-3.0.1.min.js')}}"></script>
+  <script src="{{asset('js/popper.min.js')}}"></script>
+  <script src="{{asset('js/bootstrap.min.js')}}"></script>
+  <script src="{{asset('js/jquery.easing.1.3.js')}}"></script>
+  <script src="{{asset('js/jquery.waypoints.min.js')}}"></script>
+  <script src="{{asset('js/jquery.stellar.min.js')}}"></script>
+  <script src="{{asset('js/owl.carousel.min.js')}}"></script>
+  <script src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>
+  <script src="{{asset('js/aos.js')}}"></script>
+  <script src="{{asset('js/jquery.animateNumber.min.js')}}"></script>
+  <script src="{{asset('js/bootstrap-datepicker.js')}}"></script>
+  <script src="{{asset('js/scrollax.min.js')}}"></script>
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-  <script src="js/google-map.js"></script>
-  <script src="js/main.js"></script>
-
-  <script>
-		$(document).ready(function(){
-
-		var quantitiy=0;
-		   $('.quantity-right-plus').click(function(e){
-
-		        // Stop acting like a button
-		        e.preventDefault();
-		        // Get the field name
-		        var quantity = parseInt($('#quantity').val());
-
-		        // If is not undefined
-
-		            $('#quantity').val(quantity + 1);
-
-
-		            // Increment
-
-		    });
-
-		     $('.quantity-left-minus').click(function(e){
-		        // Stop acting like a button
-		        e.preventDefault();
-		        // Get the field name
-		        var quantity = parseInt($('#quantity').val());
-
-		        // If is not undefined
-
-		            // Increment
-		            if(quantity>0){
-		            $('#quantity').val(quantity - 1);
-		            }
-		    });
-
-		});
-	</script>
-
+  <script src="{{asset('js/google-map.js')}}"></script>
+  <script src="{{asset('js/main.js')}}"></script>
   </body>
 </html>
